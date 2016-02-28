30f05eb97d30af284017030aa5155502622460fc
西山生成 <a13.nn5h@g.chuo-u.ac.jp>
2016-02-28 13:23:05 +0900

    Initial Commit

---
+++ scroll_swift3/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  scroll_swift3
+//
+//  Created by KINARI NISHIYAMA on 2016/02/28.
+//  Copyright © 2016年 KINARI NISHIYAMA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
